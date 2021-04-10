e45645de23a63686ae1e5d20c8cb7887cb36fd64
Andrzej <andrzej.przybysz01@gmail.com>
2021-04-11 01:38:54 +0200

    add more implementation to product creator

--- src/app/pages/product/components/add-product/add-product.component.ts
+++ src/app/pages/product/components/add-product/add-product.component.ts
@@ -1,3 +1,6 @@
+import { ProductResourceService } from './../../../../pms-products-sdk/api/productResource.service';
+import { CategoryResourceService } from './../../../../pms-products-sdk/api/categoryResource.service';
+import { CategoryDTO } from './../../../../pms-products-sdk/model/categoryDTO';
 import { BrandResourceService } from './../../../../pms-products-sdk/api/brandResource.service';
 import { ShopResourceService } from './../../../../pms-products-sdk/api/shopResource.service';
 import { BrandDTO } from './../../../../pms-products-sdk/model/brandDTO';
@@ -14,7 +17,6 @@ import { CurrencyDTO } from 'src/app/pms-products-sdk';
 })
 export class AddProductComponent implements OnInit {
   firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
   thirdFormGroup: FormGroup;
 
   public currencies: CurrencyDTO[] = [];
@@ -22,24 +24,31 @@ export class AddProductComponent implements OnInit {
   public shops: ShopDTO[] = [];
   public brandsNames: string[] = [];
   public shopsNames: string[] = [];
+  public imagesToAdd: string[] = [];
+  public imageToAdd: string = '';
+  public categories: CategoryDTO[] = [];
+  public categoryNames: string[] = [];
+  public categoriesToAdd: string[] = [];
+  public loading: boolean = false;
 
   constructor(
     private _formBuilder: FormBuilder,
     private currencyService: CurrencyResourceService,
     private shopService: ShopResourceService,
-    private brandService: BrandResourceService
+    private brandService: BrandResourceService,
+    private categoryService: CategoryResourceService,
+    private productService: ProductResourceService
   ) {
     this.firstFormGroup = this._formBuilder.group({
       name: ['', Validators.required],
       subtitle: [''],
       price: [],
       currency: [],
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: [''],
+      brand: [''],
+      shop: [''],
     });
     this.thirdFormGroup = this._formBuilder.group({
-      thirdCtrl: [''],
+      category: [''],
     });
   }
 
@@ -58,17 +67,50 @@ export class AddProductComponent implements OnInit {
       this.shops = res;
       this.shopsNames = this.shops.map((it) => (it.name ? it.name : ''));
     });
-  }
-
-  public create(event: Event): void {
-    event.stopPropagation();
+    this.categoryService.getAllCategoriesUsingGET().subscribe((res) => {
+      this.categories = res;
+      this.categoryNames = this.categories.map((it) =>
+        it.name ? it.name : ''
+      );
+    });
   }
 
   public brandChange(value: string): void {
     console.log(value);
+    this.firstFormGroup.get('brand')?.setValue(value);
   }
 
   public shopChange(value: string): void {
     console.log(value);
+    this.firstFormGroup.get('shop')?.setValue(value);
+  }
+
+  public addImage(event: Event): void {
+    event.stopPropagation();
+    if (this.imageToAdd.length === 0) {
+      return;
+    }
+    this.imagesToAdd.push(this.imageToAdd);
+    this.imageToAdd = '';
+  }
+
+  public categoryChange(value: string): void {
+    console.log(value);
+    this.thirdFormGroup.get('category')?.setValue(value);
+  }
+
+  public addCategory(event: Event): void {
+    event.stopPropagation();
+    if (this.thirdFormGroup.get('category')?.value.length === 0) {
+      return;
+    }
+    this.categoriesToAdd.push(this.thirdFormGroup.get('category')?.value);
+    this.thirdFormGroup.get('category')?.setValue('');
+  }
+
+  public create(event: Event): void {
+    event.stopPropagation();
+    this.loading = true;
+    //todo
   }
 }
